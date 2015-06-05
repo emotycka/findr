1c45544fd5006b6d468b9976aa208d86b02ee8e1
Eli Motycka <elimotycka@Eli-Motyckas-MacBook-Pro.local>
2015-06-05 13:02:42 -0400

    Initial Commit

---
+++ MapsComponent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapsComponent
+//
+//  Created by Eli Motycka on 6/5/15.
+//  Copyright (c) 2015 Findr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
