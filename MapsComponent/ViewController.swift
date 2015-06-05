f4c27ccd383a74b52da5fd3a47230357fcc7ae06
Eli Motycka <elimotycka@Eli-Motyckas-MacBook-Pro.local>
2015-06-05 17:40:04 -0400

    Next commit

--- MapsComponent/ViewController.swift
+++ MapsComponent/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import MapKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var camera = GMSCameraPosition.cameraWithLatitude(41.8262, longitude: -71.4032, zoom: 12)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        var demoMarker = GMSMarker()
+        demoMarker.position = camera.target
+        demoMarker.snippet = "I lost my Canada Goose jacket!"
+        demoMarker.appearAnimation = kGMSMarkerAnimationPop
+        demoMarker.map = mapView
+        self.view = mapView
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+}
+
+class Waypoint: Entry, Printable
+{
+    
+    var info: String? {
+        set{
+            attributes["desc"] = newValue
+        }
+        get{
+            return attributes["desc"]
+        }
     }
+}
 
 
+class Entry: NSObject, Printable
+{
+    var links = [Link]()
+    var attributes = [String:String]()
+    
+    var name: String? {
+        set{
+            attributes["name"] = newValue
+        }
+        get{
+            return attributes["name"]
+        }
+    }
+    
+    override var description: String {
+        var descriptions = [String]()
+        if attributes.count > 0 {descriptions.append("attributes=\(attributes)")}
+        if links.count > 0 { descriptions.append("links=\(links)")}
+        return " ".join(descriptions)
+    }
+}
+
+class Link: Printable
+{
+    var href: String
+    var linkattributes = [String:String]()
+    
+    init(href: String) {
+        self.href = href
+    }
+    
+    var url: NSURL? { return NSURL(string: href) }
+    var text: String? { return linkattributes["text"] }
+    var type: String? { return linkattributes["type"] }
+    
+    var description: String {
+        var descriptions = [String]()
+        descriptions.append("href=\(href)")
+        if linkattributes.count > 0 {descriptions.append("linkattributes=\(linkattributes)")}
+        return "[" + " ".join(descriptions) + "]"
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+#get Maps API from cocoa pods git repo
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+
+
+target 'MapsComponent' do
+pod 'GoogleMaps'
+end
+
+
